8f947dcdad15c228173024fcc6486e54f4b4538d
groorj <gustavo.aguiar@gmail.com>
2023-06-30 22:48:51 -0400

    Improving build

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 HTTP_PORT=8215
-APP_NAME="url-shortener-app"
-docker build -t ${APP_NAME}
-docker run -p ${HTTP_PORT}:${HTTP_PORT} -e PORT=${HTTP_PORT} ${APP_NAME}
+APP_NAME="my-url-shortener-app"
+
+echo "docker container rm --force \"${APP_NAME}\""
+echo "docker build -t ${APP_NAME} ."
+#echo "docker run -p ${HTTP_PORT}:${HTTP_PORT} -e PORT=${HTTP_PORT} ${APP_NAME}"
+echo "docker run -d --name ${APP_NAME} --sysctl net.ipv4.ip_unprivileged_port_start=0 --publish ${HTTP_PORT}:${HTTP_PORT} ${APP_NAME}"
+
+# kill running
+#docker ps -aqf "name=${APP_NAME}"
+
+docker container rm --force "${APP_NAME}"
+docker build -t ${APP_NAME} .
+docker run -d --name ${APP_NAME} --sysctl net.ipv4.ip_unprivileged_port_start=0 --publish ${HTTP_PORT}:${HTTP_PORT} ${APP_NAME}
 
 # End;
