a8c2512115dbfd5dadd64f6db1a995a0b9f46220
groorj <gustavo.aguiar@gmail.com>
2023-07-02 23:34:11 -0400

    Adding DynamoDB

--- app-url-shortener.py
+++ app-url-shortener.py
@@ -15,14 +15,16 @@ consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
 # vars
-table_name = 'redirections_table'
+table_name = 'redirections_table2'
 aws_region = 'us-east-1'
 http_port = 8215
 server_address = '0.0.0.0'
 
+logger.debug("os.environ: [%s]", os.environ)
+
 # Connect to DynamoDB
-#dynamodb = boto3.resource('dynamodb', region_name=aws_region)
-#table = dynamodb.Table(table_name)
+dynamodb = boto3.resource('dynamodb', region_name=aws_region)
+table = dynamodb.Table(table_name)
 
 class RedirectionHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -39,23 +41,23 @@ class RedirectionHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'Hello!')
         else:
-            logger.debug("else -> path [%s]", path)
-            self.send_response(302)
-            self.send_header('Location', '/caiu-no-else')
-            self.end_headers()
+            # logger.debug("else -> path [%s]", path)
+            # self.send_response(302)
+            # self.send_header('Location', '/caiu-no-else')
+            # self.end_headers()
 
-#            # Retrieve redirection mappings from DynamoDB
-#            response = table.get_item(Key={'source_uri': path})
-#            if 'Item' in response:
-#                item = response['Item']
-#                new_path = item['target_uri']
-#                self.send_response(301)
-#                self.send_header('Location', new_path)
-#                self.end_headers()
-#            else:
-#                self.send_response(302)
-#                self.send_header('Location', 'https://github.com/groorj/aws-url-shortener')
-#                self.end_headers()
+           # Retrieve redirection mappings from DynamoDB
+           response = table.get_item(Key={'source_uri': path})
+           if 'Item' in response:
+               item = response['Item']
+               new_path = item['target_uri']
+               self.send_response(301)
+               self.send_header('Location', new_path)
+               self.end_headers()
+           else:
+               self.send_response(302)
+               self.send_header('Location', 'https://github.com/groorj/aws-url-shortener')
+               self.end_headers()
 
 # Set up the HTTP server
 port = int(os.environ.get('PORT', http_port))
